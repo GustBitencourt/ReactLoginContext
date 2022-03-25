b3728a3da51cf345b2e2f66c0deca31cdb5ceb1c
GustBitencourt <gubiten@gmail.com>
2022-03-25 19:19:12 -0300

    regras do provider separada de routes, apenas em seu contexto

--- src/Context/auth.js
+++ src/Context/auth.js
@@ -1,3 +1,23 @@
-import { createContext } from "react";
+import { useState, createContext } from "react";
 
 export const AuthContext = createContext();
+
+export const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+
+  const login = (email, password) => {
+    console.log("LoginAuthInRoutes", { email, password });
+    setUser({ id: "123", email });
+  };
+
+  const logout = () => {};
+
+
+  return (
+    <AuthContext.Provider
+      value={{ authenticated: !!user, user, login, logout }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
+};
--- src/Routes/Routes.js
+++ src/Routes/Routes.js
@@ -1,36 +1,24 @@
-import { useState } from "react";
+
 import {
   BrowserRouter,
   Route,
   Routes,
 } from "react-router-dom";
-import { AuthContext } from "../Context/auth";
+import { AuthProvider } from "../Context/auth";
 import Home from "../Pages/Home";
 import LoginPage from "../Pages/LoginPage";
 
 
 
 export const AppRoutes = () => {
-  const [user, setUser] = useState(null);
-
-  const login = ( email, password ) => {    
-    console.log('LoginAuthInRoutes', {email, password});
-    setUser({ id: '123', email })
-
-  }
-
-  const logout = () => {
-
-  }
-
   return (
     <BrowserRouter>
-      <AuthContext.Provider value={{ authenticated: !!user, user, login, logout }} >
+      <AuthProvider>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/login" element={<LoginPage />} />
         </Routes>
-      </AuthContext.Provider>
+      </AuthProvider>
     </BrowserRouter>
   );
 };
