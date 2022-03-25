c140209f56427898fe0335a96591613b9d9202c0
GustBitencourt <gubiten@gmail.com>
2022-03-25 19:09:05 -0300

    test do context funcionando

--- src/Pages/LoginPage/index.js
+++ src/Pages/LoginPage/index.js
@@ -1,19 +1,29 @@
-import { useState } from "react";
+import { useState, useContext } from "react";
+import { AuthContext } from "../../Context/auth";
+
 
 import "./style.css"
 
 const LoginPage = () => {
+  //informações vinda do context
+  const { authenticated, login } = useContext(AuthContext);
+
+
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
   const handleSubmit = (event) => {
     event.preventDefault();
     console.log('LoginPage', {email, password} );
+
+    //integrando com o contexto e API
+    login(email, password);
   }
 
   return (
     <div id="login">
       <h1 className="title">Login</h1>
+      <p>{String(authenticated)}</p>
 
       <form className="form" onSubmit={handleSubmit}>
         <div className="field">
--- src/Routes/Routes.js
+++ src/Routes/Routes.js
@@ -1,18 +1,36 @@
+import { useState } from "react";
 import {
   BrowserRouter,
   Route,
   Routes,
 } from "react-router-dom";
+import { AuthContext } from "../Context/auth";
 import Home from "../Pages/Home";
 import LoginPage from "../Pages/LoginPage";
 
+
+
 export const AppRoutes = () => {
+  const [user, setUser] = useState(null);
+
+  const login = ( email, password ) => {    
+    console.log('LoginAuthInRoutes', {email, password});
+    setUser({ id: '123', email })
+
+  }
+
+  const logout = () => {
+
+  }
+
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<LoginPage />} />
-      </Routes>
+      <AuthContext.Provider value={{ authenticated: !!user, user, login, logout }} >
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<LoginPage />} />
+        </Routes>
+      </AuthContext.Provider>
     </BrowserRouter>
   );
 };
